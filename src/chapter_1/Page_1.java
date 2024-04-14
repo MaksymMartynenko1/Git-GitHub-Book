9bc7d1f6b43afda04b086e1424373c1d296a9107
Maksym <martynenkomaksim1983@gmail.com>
2024-04-14 11:01:46 -0400

    added new print

--- src/chapter_1/Page_1.java
+++ src/chapter_1/Page_1.java
@@ -3,6 +3,6 @@ package chapter_1;
 public class Page_1 {
     public static void main(String[] args) {
         System.out.println("This is additional lines ");
-        System.out.println("THis added from g");
+        System.out.println("THis added from git_day2 branch");
     }
 }
